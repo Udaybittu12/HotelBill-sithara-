bd0d58103bbc54e296db8c0c188383214d9ed34c
bittu <bittu@HP>
2024-06-06 15:19:58 +0530

    Add project files.

---
+++ HotelBill/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="HotelBill.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Customer List</title>
+</head>
+<body>
+    <form id="from1" runat="server">
+        <h1>Welcome To HOTEL SITHARA</h1>
+    <asp:Label ID="Label1" runat="server" Text="Customer Name"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+    <asp:Label ID="Label2" runat="server" Text="Room Type"></asp:Label>
+    <asp:RadioButton ID="RadioButton1" runat="server" GroupName="s" AutoPostBack="true" Text="AC" OnCheckedChanged="RadioButton1_CheckedChanged" />
+    <asp:RadioButton ID="RadioButton2" runat="server" GroupName="s" AutoPostBack="true" Text="NON AC" OnCheckedChanged="RadioButton2_CheckedChanged" /><br />
+    <asp:Panel ID="Panel1" runat="server" Visible="False">
+     <asp:Label ID="Label3" runat="server" Text="Services"></asp:Label><br />
+    <asp:CheckBox ID="CheckBox1" runat="server" Text="Wifi" /><br />
+    <asp:CheckBox ID="CheckBox2" runat="server" Text="Hot Water" /><br />
+    <asp:CheckBox ID="CheckBox3" runat="server" Text="Tv" /><br />
+    <asp:CheckBox ID="CheckBox4" runat="server" Text="Extra Bed" /><br />
+    </asp:Panel>
+    <br />
+    <asp:Label ID="Label6" runat="server" Text="Food"></asp:Label>
+    <asp:RadioButton ID="RadioButton3" runat="server" Text="With Meals" GroupName="k" />
+    <asp:RadioButton ID="RadioButton4" runat="server" Text="Without Meals" GroupName="k" /><br />
+    <asp:Label ID="Label4" runat="server" Text="Number Of Days"></asp:Label>
+    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+    <asp:Label ID="Label5" runat="server" Text="Bill Amount"></asp:Label>
+    <asp:TextBox ID="TextBox3" runat="server" ReadOnly="True"></asp:TextBox><br />
+    <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" /><br />
+        <br />
+        click here for << <asp:Button ID="Button2" runat="server" Text="All CUstomer Details" BorderStyle="Ridge" BorderColor="Black" OnClick="Button2_Click"  />
+        <asp:GridView ID="GridView1" runat="server" ></asp:GridView>
+        </form>
+</body>
+</html>
---
+++ HotelBill/WebForm1.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace HotelBill
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void BindCustomers()
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string name = TextBox1.Text;
+            double cost = 1;
+            double services = 1;
+            if (CheckBox1.Checked == true)
+            {
+                services = services + 200;
+            }
+            if (CheckBox2.Checked == true)
+            {
+                services = services + 100;
+            }
+            if (CheckBox3.Checked == true)
+            {
+                services = services + 100;
+            }
+            if (CheckBox4.Checked == true)
+            {
+                services = services + 400;
+            }
+            int days = Convert.ToInt32(TextBox2.Text);
+            double meals = 1;
+            if (RadioButton3.Checked == true)
+            {
+                meals = 300;
+            }
+            else if (RadioButton4.Checked == true)
+            {
+                meals = 0;
+            }
+            if (RadioButton1.Checked == true)
+            {
+                cost = 1500;
+            }
+            else if (RadioButton2.Checked == true)
+            {
+                cost = 800;
+            }
+            double billamt = days * (meals + cost + services);
+            TextBox3.Text = billamt.ToString();
+            // customer name and bill storing in database
+            string connectionString = ConfigurationManager.ConnectionStrings["hotelbill"].ToString();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("sp_hbill", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Cname", TextBox1.Text);
+                command.Parameters.AddWithValue("@cbill", TextBox3.Text);
+                int k = command.ExecuteNonQuery();
+                if (k != 0) { Response.Write("customer registered successfulyy"); }
+                else { Response.Write("customer not registered due to error"); }
+                connection.Close();
+            }
+        }
+
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Panel1.Visible = true;
+            TextBox2.Text = TextBox3.Text = "";
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Panel1.Visible = false;
+            TextBox2.Text = TextBox3.Text = "";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["hotelbill"].ConnectionString;
+            string query = "select*from hotel";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        GridView1.DataSource = dataTable;
+                        GridView1.DataBind();
+                    }
+
+                }
+            }
+
+        }
+    }
+}
+    
